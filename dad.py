cde28632ceebe2a96d131d7dbcb923cc686d81b1
Atharva Jayant Paradkar <103434421+AtharvaParadkar@users.noreply.github.com>
2023-05-19 10:27:48 +0530

    Delete dad.py

--- dad.py
+++
@@ -1,6 +0,0 @@
-numbers = [5, 2, 9, 1, 7]
-unique_numbers = set(numbers)
-unique_numbers.remove(max(unique_numbers))
-second_largest = max(unique_numbers)
-
-print("Second Largest Number:", second_largest)
